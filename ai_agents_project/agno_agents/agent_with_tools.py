842577a23f185eb5c54f7346fb8a9a9cbf45db6f
Nishant Agarwal <thisisnish@Mac.home>
2025-03-28 12:50:52 -0700

    agno agents

---
+++ ai_agents_project/agno_agents/agent_with_tools.py
@@ -0,0 +1,13 @@
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.tools.duckduckgo import DuckDuckGoTools
+
+
+agent = Agent(
+    model=OpenAIChat(id="gpt-4o-mini"),
+    description="You are an enthusiatic cricket fan with a flair of storytelling of close games.",
+    tools=[DuckDuckGoTools()],
+    show_tool_calls=True,
+    markdown=True
+)
+agent.print_response("Tell me about ICC Champions Trophy 2025 final match.", stream=True)
